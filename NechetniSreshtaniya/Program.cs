d42a26c3c7c208ad48849efc12cf8dcaedf41b8a
DiyanTrifonov <diyantrifonov5@gmail.com>
2023-04-30 11:19:15 +0300

    First commit

---
+++ NechetniSreshtaniya/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NechetniSreshtaniya
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().ToLower().Split(' ').ToArray();
+            Dictionary<string, int> statistika = new Dictionary<string, int>();
+
+            foreach (var item in words)
+            {
+                if (statistika.ContainsKey(item))
+                {
+                    statistika[item]++;
+                }
+                else
+                {
+                    statistika.Add(item,1);
+                }
+            }
+
+            foreach (var item in statistika)
+            {
+                if (item.Value % 2 == 1)
+                {
+                    Console.WriteLine($"{item.Key} -> {item.Value}");
+                }
+            }
+        }
+    }
+}
---
+++ TelefonenUkazatel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelefonenUkazatel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> phoneBook = new Dictionary<string, string>();
+            string input = Console.ReadLine();
+
+            while (input != "END")
+            {
+                string[] tokens = input.Split();
+                string command = tokens[0];
+                string name = tokens[1];
+
+                switch (command)
+                {
+                    case "A":
+                        string phone = tokens[2];
+                        phoneBook.Add(name, phone); break;
+
+                    case "S":
+                        if (phoneBook.ContainsKey(name))
+                        {
+                            Console.WriteLine($"{name} -> {phoneBook[name]}");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"Контактът {name} не съществува!");
+                        }
+                        break;
+
+                    default:
+                        break;
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
